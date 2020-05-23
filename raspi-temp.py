89d4165fe795e8c3b22e5f68776140f2c0a5a346
Francisco Javier Morales <fmorales@ar.ibm.com>
2020-05-23 19:34:53 -0300

    Update de indentado

--- raspi-temp.py
+++ raspi-temp.py
@@ -1,10 +1,10 @@
 import os
 
 def raspi_temp():
-  temp = os.popen("vcgencmd measure_temp").readline()
-  temp = temp.replace("'C","")
-  temp = temp.replace("temp=","")
-  temp = float(temp)
-  return temp
+    temp = os.popen("vcgencmd measure_temp").readline()
+    temp = temp.replace("'C","")
+    temp = temp.replace("temp=","")
+    temp = float(temp)
+    return temp
 
 print("%.2f" % raspi_temp())
